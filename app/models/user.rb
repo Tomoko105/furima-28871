2a5963d71e3617fd990ca11db5af74610276314d
Tomoko105 <on82.ino.tomoko@gmail.com>
2020-09-08 11:27:01 +0900

    ユーザー管理機能を実装

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable
+      
+  validates :nickname, :birthday, presence: true
+  validates :email, presence: true,uniqueness: true,format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, presence: true, length: { maximum: 6 }, confirmation: true,format: { with: VALID_PASSWORD_REGEX }
+
+  
+  validates :firstname, :lastname, presence: true, format: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+  #validates :lastname, presence: true,
+  validates :kana_firstname, :kana_lastname, presence: true, format: /\A[ァ-ヶー－]+\z/
+  #validates :kana_lastname, presence: true,
+  #validates :birthday, presence: true,
+
+
+end
