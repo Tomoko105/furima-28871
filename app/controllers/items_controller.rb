1f7ec184c9b7f7cd3192dd60221bf336bc1fb387
Tomoko105 <on82.ino.tomoko@gmail.com>
2020-09-18 12:25:06 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :select, only: [:show, :edit, :update, :destroy]
-  before_action :user_check, only: [:update,:edit,:destroy]
+  before_action :user_check, only: [:update, :edit, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
