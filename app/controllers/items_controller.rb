a523a44bd199a6078fc688b01d454cbeaa567cd2
Tomoko105 <on82.ino.tomoko@gmail.com>
2020-09-18 10:59:41 +0900

    商品情報編集機能実装(修正１)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :select, only: [:show, :edit, :update]
-  before_action :user_check, only: [:update]
+  before_action :user_check, only: [:update,:edit]
   def index
     @items = Item.all.order('created_at DESC')
   end
